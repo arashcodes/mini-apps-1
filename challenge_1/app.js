eedd5b0488c16ce41f42698f781be7278701932c
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-11-18 11:40:40 -0800

    Adds functionality that prints the square that is clicked

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
 
 
+let squares = document.getElementsByClassName("square");
+
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].onclick = function(event){
+    var idx = event.toElement.attributes.id.nodeValue;
+    console.log(idx);
+  };
+}
