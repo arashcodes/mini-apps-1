4a339ced383bef0246f83a25c730f67b9c28cee9
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-11-19 15:26:43 -0800

    Completes the post request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
-
-
+document.getElementById('my-table').onsubmit = function(event) {
+  event.preventDefault()
+  let input = document.getElementById('textarea').value;
+  console.log('posted from app')
+  fetch('http://localhost:3000/data', {
+    method: 'POST',
+    body: JSON.stringify({key: input}),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  })
+  .then( (response) => { 
+    console.log(response.body)
+ })
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, '/client')));
+
+app.post('/data', (req, res) => {
+  res.send(console.log(req.body));
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
